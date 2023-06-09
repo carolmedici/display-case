13d874c42fd012ad56d9e0ab93706d4106a4afbe
carolmedici <carolina.obstetriz@gmail.com>
2023-06-09 17:38:59 -0300

    :dizzy: view discount

--- src/assets/Product/Modal/index.jsx
+++ src/assets/Product/Modal/index.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 import ModalContent from '../ModalContent';
+import "./Modal.css"
 
-export default function Modal({discount, price, description, title, image}) {
+export default function Modal({ discount, price, description, title, image }) {
   const [showModal, setShowModal] = useState(false);
+
+  
+  const discountTag = discount ? <div className="text-red-500 text-right animate-swing">Discount!!</div> : null;
+
   return (
     <>
       <button className='relative inset-x-3/4 rounded-2xl botton-1 my-2 bg-gray-200 p-2 hover:bg-gray-400 hover:text-white' onClick={() => setShowModal(true)}>
@@ -13,6 +18,7 @@ export default function Modal({discount, price, description, title, image}) {
         <ModalContent discount={discount} price={price} title={title} image={image} description={description} onClose={() => setShowModal(false)} />,
         document.body
       )}
+      {discountTag}
     </>
   );
 }
